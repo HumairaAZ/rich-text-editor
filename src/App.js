10bd2e41092e9d1e14968e656b94a6f0900f5e15
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 00:20:26 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+// src/App.js
+
 import React from 'react';
 import Editor from './components/Editor';
+import { AppBar, Toolbar, Typography, Container, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  container: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Rich Text Editor</h1>
-      </header>
-      <main>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Rich Text Editor
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.container}>
         <Editor />
-      </main>
+      </Container>
     </div>
   );
 }
