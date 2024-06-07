cc7ef4de2070a9533c47a01cccfc946afa9af101
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-08 00:46:39 +0600

    Update RichTextEditorToolbar.js

--- src/components/RichTextEditorToolbar.js
+++ src/components/RichTextEditorToolbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
-import { FormatBold, FormatItalic, FormatUnderlined, Image, Code, TableChart } from '@material-ui/icons';
+import { AppBar, Toolbar, IconButton, Typography, Select, MenuItem } from '@material-ui/core';
+import { FormatBold, FormatItalic, FormatUnderlined, Image, Code, TableChart, Undo, Redo } from '@material-ui/icons';
 
-const RichTextEditorToolbar = ({ onBold, onItalic, onUnderline, onImage, onCode, onTable }) => (
+const RichTextEditorToolbar = ({ onBold, onItalic, onUnderline, onImage, onCode, onTable, onUndo, onRedo, onBlockTypeChange, onFontSizeChange }) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6">Rich Text Editor</Typography>
@@ -24,6 +24,34 @@ const RichTextEditorToolbar = ({ onBold, onItalic, onUnderline, onImage, onCode,
       <IconButton color="inherit" onClick={onTable} aria-label="Insert Table">
         <TableChart />
       </IconButton>
+      <IconButton color="inherit" onClick={onUndo} aria-label="Undo">
+        <Undo />
+      </IconButton>
+      <IconButton color="inherit" onClick={onRedo} aria-label="Redo">
+        <Redo />
+      </IconButton>
+      <Select
+        defaultValue=""
+        onChange={(e) => onBlockTypeChange(e.target.value)}
+        displayEmpty
+        inputProps={{ 'aria-label': 'Block Type' }}
+      >
+        <MenuItem value="">Block Type</MenuItem>
+        <MenuItem value="header-one">Header One</MenuItem>
+        <MenuItem value="header-two">Header Two</MenuItem>
+        <MenuItem value="unstyled">Normal</MenuItem>
+      </Select>
+      <Select
+        defaultValue=""
+        onChange={(e) => onFontSizeChange(e.target.value)}
+        displayEmpty
+        inputProps={{ 'aria-label': 'Font Size' }}
+      >
+        <MenuItem value="">Font Size</MenuItem>
+        <MenuItem value="FONT_SIZE_12">12px</MenuItem>
+        <MenuItem value="FONT_SIZE_16">16px</MenuItem>
+        <MenuItem value="FONT_SIZE_20">20px</MenuItem>
+      </Select>
     </Toolbar>
   </AppBar>
 );
