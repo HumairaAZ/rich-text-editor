0ce4291bfe7f3ff02854ab15075fbcd2d26cc75d
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 22:20:53 +0600

    Delete App.test.js

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Rich Text Editor header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Rich Text Editor/i);
-  expect(headerElement).toBeInTheDocument();
-});
