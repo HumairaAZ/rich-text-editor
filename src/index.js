b5ca84c6a0e072ef150cc77525edee603d414ba7
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-07 23:27:28 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RichTextEditor from './components/RichTextEditor';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RichTextEditor />
   </React.StrictMode>,
   document.getElementById('root')
 );
